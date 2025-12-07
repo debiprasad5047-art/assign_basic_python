276395cb2b3de92194d13a2f75b0988bb2b24213
Debiprasad Parida <debiprasad5047@gmail.com>
2025-12-07 22:31:05 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,7 @@
+num1=int(input("Enter 1st number:"))
+num2=int(input("Enter 2nd number:"))
 
+print(f"addition:{num1+num2}")
+print(f"subtraction:{num1-num2}")
+print(f"multiplication:{num1*num2}")
+print(f"division:{num1/num2}")
